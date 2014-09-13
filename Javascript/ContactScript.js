1b203e9c63cd1a46adec7242a803b856a7e9a3ae
M138A <0896375@hr.nl>
2014-09-13 15:55:35 +0200

    Minor edit.

--- Javascript/ContactScript.js
+++ Javascript/ContactScript.js
@@ -15,7 +15,7 @@ function CheckMailAdress() {
         }
         else {
             document.getElementById("error").style.display = "block";
-            document.getElementById("error").innerHTML += "Mail niet geldig. Vul een geldig e-mail adres in.<br/>";
+            document.getElementById("error").innerHTML = "Mail niet geldig. Vul een geldig e-mail adres in.<br/>";
             return false;
         }
     }
@@ -23,55 +23,34 @@ function CheckMailAdress() {
 function CheckIfValueEmpty(ID) {
     switch (ID) {
         case "Naam":
-            var val = document.getElementById("Naam");
-            if (val.value === val.defaultValue) {
+            var value = document.getElementById(ID).value;
+            if (value !== "" && /^[a-zA-Z]+$/.test(value)) {
                 return true;
             }
             else {
-
-                var value = document.getElementById(ID).value;
-                if (value !== "" && /^[a-zA-Z]+$/.test(value)) {
-                    document.getElementById("error").style.display = "None";
-                    return true;
-                }
-                else {
-                    document.getElementById("error").style.display = "block";
-                    document.getElementById("error").innerHTML += "Naam niet geldig. Vul een geldige naam in.<br/>";
-                }
+                document.getElementById("error").style.display = "block";
+                document.getElementById("error").innerHTML = "Naam niet geldig. Vul een geldige naam in.<br/>";
             }
             break;
         case "Bericht":
-            var val = document.getElementById("Bericht");
-            if (val.value === val.defaultValue) {
+            var value = document.getElementById(ID).value;
+            if (value !== "" && value.length < 501) {
                 return true;
             }
             else {
-                var value = document.getElementById(ID).value;
-                if (value !== "" && value.length < 501) {
-                    document.getElementById("error").style.display = "None";
-                    return true;
-                }
-                else {
-                    document.getElementById("error").style.display = "block";
-                    document.getElementById("error").innerHTML += "Bericht is niet ingevuld of te lang.<br/>";
-                }
+                document.getElementById("error").style.display = "block";
+                document.getElementById("error").innerHTML = "Bericht is niet ingevuld of te lang.<br/>";
+                return false;
             }
             break;
         case "Mail":
-            var val = document.getElementById("Mail");
-            if (val.value === val.defaultValue) {
+            var value = document.getElementById(ID).value;
+            if (value !== "") {
                 return true;
             }
             else {
-                var value = document.getElementById(ID).value;
-                if (value !== "") {
-                    document.getElementById("error").style.display = "None";
-                    return true;
-                }
-                else {
-                    document.getElementById("error").style.display = "block";
-                    document.getElementById("error").innerHTML = "Mail niet geldig<br/>";
-                }
+                document.getElementById("error").style.display = "block";
+                document.getElementById("error").innerHTML = "Mail niet geldig<br/>";
             }
             break;
     }
@@ -79,11 +58,11 @@ function CheckIfValueEmpty(ID) {
 function CheckContactForm() {
     if (CheckMailAdress() && CheckIfValueEmpty('Naam') && CheckIfValueEmpty('Mail') && CheckIfValueEmpty('Bericht')) {
         return true;
-
     }
     else {
         document.getElementById("error").style.display = "block";
-        document.getElementById("error").innerHTML += "Er waren nog fouten in het formulier.<br/>";
+        document.getElementById("error").innerHTML = "";
+        document.getElementById("error").innerHTML += "Er zijn nog fouten in het formulier.<br/>";
         return false;
     }
 }
