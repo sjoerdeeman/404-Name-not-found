804becf309251c7229ff9dac989ac989ab95a3ec
M138A <0896375@hr.nl>
2014-09-15 11:36:42 +0200

    Finished Contact Form.
    Contact form is done. Deleted not needed Javascript file.

--- Javascript/ContactScript.js
+++ Javascript/ContactScript.js
@@ -5,15 +5,19 @@
  */
 function CheckMailAdress() {
     var val = document.getElementById("Mail");
+    //check if value has changed
     if (val.value === val.defaultValue) {
         return true;
     }
     else {
         var InputAdress = document.getElementById("Mail").value;
+        //check if mail is valid
         if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(InputAdress)) {
+            //Mail is valid
             return true;
         }
         else {
+            //Mail is not valid and display error message
             document.getElementById("error").style.display = "block";
             document.getElementById("error").innerHTML = "Mail niet geldig. Vul een geldig e-mail adres in.<br/>";
             return false;
@@ -25,27 +29,35 @@ function CheckIfValueEmpty(ID) {
         case "Naam":
             var value = document.getElementById(ID).value;
             if (value !== "" && /^[a-zA-Z]+$/.test(value)) {
+                //Naam is valid
                 return true;
             }
             else {
+                //Naam is not valid and display error message
                 document.getElementById("error").style.display = "block";
                 document.getElementById("error").innerHTML = "Naam niet geldig. Vul een geldige naam in.<br/>";
+                return false;
             }
             break;
         case "Bericht":
             var value = document.getElementById(ID).value;
+            //check if Bericht is not empty and not longer than 500 characters.
             if (value !== "" && value.length < 501) {
+                //Bericht is valid
                 return true;
             }
             else {
+                //Bericht is not valid and display error message
                 document.getElementById("error").style.display = "block";
                 document.getElementById("error").innerHTML = "Bericht is niet ingevuld of te lang.<br/>";
                 return false;
             }
             break;
         case "Mail":
+            //Check if mail is not empty
             var value = document.getElementById(ID).value;
             if (value !== "") {
+                //Mail is not empty
                 return true;
             }
             else {
@@ -56,10 +68,13 @@ function CheckIfValueEmpty(ID) {
     }
 }
 function CheckContactForm() {
+    //check if all the values in the form are valid
     if (CheckMailAdress() && CheckIfValueEmpty('Naam') && CheckIfValueEmpty('Mail') && CheckIfValueEmpty('Bericht')) {
+        //Everything is good
         return true;
     }
     else {
+        //There was something wrong and display error message
         document.getElementById("error").style.display = "block";
         document.getElementById("error").innerHTML = "";
         document.getElementById("error").innerHTML += "Er zijn nog fouten in het formulier.<br/>";
