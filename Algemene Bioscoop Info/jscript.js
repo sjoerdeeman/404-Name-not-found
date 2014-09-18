ac3faee11b847c89f8f7eb9397beff2e59ef0da3
sjoerdeeman <sjoerd.eeman@gmail.com>
2014-09-18 15:46:01 +0200

    Ons over edited

--- Algemene Bioscoop Info/jscript.js
+++ Algemene Bioscoop Info/jscript.js
@@ -1,1 +1,10 @@
-var henk = "leuke website";
+    document.onmousedown=disableclick;
+    status="Right Click Disabled";
+    Function disableclick(event)
+    {
+    	if(event.button==2)
+    	{
+    		alert(status);
+    		return false;    
+    	}
+    }
