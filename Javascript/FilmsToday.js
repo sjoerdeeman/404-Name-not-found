8ac5a28e671f9097a92e948b85d9c73909cb1b5a
M138A <0896375@hr.nl>
2014-09-19 20:12:22 +0200

    Edit Homage

--- Javascript/FilmsToday.js
+++ Javascript/FilmsToday.js
@@ -1,38 +1,39 @@
+function SetCurrentFilms() {
+    var date = new Date();
+    var h = date.getHours();
+    var n = date.getMinutes();
+    if (n < 10) {
+        n = "0" + n;
+    }
+    var TimeCode = h + "" + n;
+      if (TimeCode < "2000" & TimeCode >= 0800) {
+        document.getElementById("2000").style.display = "block";
+        document.getElementById("2030").style.display = "block";
+        document.getElementById("2100").style.display = "block";
+        document.getElementById("2200").style.display = "block";
+        document.getElementById("2230").style.display = "block";
 
-var date = new Date();
-var h = date.getHours();
-var n = date.getMinutes();
-if (n < 10) {
-    n = "0" + n;
-}
-var TimeCode = "0700";
-if (TimeCode < "2000" & TimeCode >= 0800) {
-    document.getElementById("2000").style.display = "block";
-    document.getElementById("2030").style.display = "block";
-    document.getElementById("2100").style.display = "block";
-    document.getElementById("2200").style.display = "block";
-    document.getElementById("2230").style.display = "block";
+    }
+    else if (TimeCode < "2030" & TimeCode >= 0800) {
 
-}
-else if (TimeCode < "2030" & TimeCode >= 0800) {
-    
-    document.getElementById("2030").style.display = "block";
-    document.getElementById("2100").style.display = "block";
-    document.getElementById("2200").style.display = "block";
-    document.getElementById("2230").style.display = "block";
-}
-else if (TimeCode < "2100" & TimeCode >= 0800) {
-    document.getElementById("2100").style.display = "block";
-    document.getElementById("2200").style.display = "block";
-    document.getElementById("2230").style.display = "block";
-}
-else if (TimeCode < "2200" & TimeCode >= 0800) {
-    document.getElementById("2200").style.display = "block";
-    document.getElementById("2230").style.display = "block";
-}
-else if (TimeCode < "2230" & TimeCode >= 0800) {
-    document.getElementById("2230").style.display = "block";
-}
-else {
-    document.getElementById("GeenFilms").style.display = "block";
+        document.getElementById("2030").style.display = "block";
+        document.getElementById("2100").style.display = "block";
+        document.getElementById("2200").style.display = "block";
+        document.getElementById("2230").style.display = "block";
+    }
+    else if (TimeCode < "2100" & TimeCode >= 0800) {
+        document.getElementById("2100").style.display = "block";
+        document.getElementById("2200").style.display = "block";
+        document.getElementById("2230").style.display = "block";
+    }
+    else if (TimeCode < "2200" & TimeCode >= 0800) {
+        document.getElementById("2200").style.display = "block";
+        document.getElementById("2230").style.display = "block";
+    }
+    else if (TimeCode < "2230" & TimeCode >= 0800) {
+        document.getElementById("2230").style.display = "block";
+    }
+    else {
+        document.getElementById("GeenFilms").style.display = "block";
+    }
 }
