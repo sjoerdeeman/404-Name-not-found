a89a99d384351da83c104d8a98492006bd780352
M138A <0896375@hr.nl>
2014-09-20 13:02:45 +0200

    Homepage

--- Javascript/FilmsToday.js
+++ Javascript/FilmsToday.js
@@ -37,3 +37,18 @@ function SetCurrentFilms() {
         document.getElementById("GeenFilms").style.display = "block";
     }
 }
+function ShowMoreOnHover(ID) {
+    var ID = document.getElementById(ID).id;
+    var NewID = "More" + "" + ID;
+    
+    document.getElementById(NewID).style.display = "block";
+    document.getElementById('NoFilmHover').style.dislay = "none"
+}
+
+function HideMe(IDToHide) {
+    document.getElementById("More2000").style.display = "none";
+    document.getElementById("More2030").style.display = "none";
+    document.getElementById("More2100").style.display = "none";
+    document.getElementById("More2200").style.display = "none";
+    document.getElementById("More2230").style.display = "none";
+}
