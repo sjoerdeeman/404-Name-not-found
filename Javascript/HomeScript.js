f9909737624fff08f69e210854f82f3747d42d4e
M138A <0896375@hr.nl>
2014-10-28 10:04:54 +0100

    Fixed Serveral errors

--- Javascript/HomeScript.js
+++ Javascript/HomeScript.js
@@ -22,12 +22,11 @@ function LoadSlider(WIDTH) {
 
 }
 function LoadAgenda() {
-
     var date = new Date();
     var Today = date.getDay();
     var FilmsID = [
         ["<strong>Finding Fanny</strong><br/> <img onmousedown=\"javascript:ShowOverlay('Finding Fanny')\" src='http://upload.wikimedia.org/wikipedia/en/3/35/Finding_Fanny_Theatrical_release_poster.jpg' alt='Finding Fanny' width='100' heigth='200'>"],
-        ["<strong>Novemberman</strong><br/> <img onmousedown=\"javascript:ShowOverlay('Novemberman')\" src='http://m.929jackfm.ca/files/VVS_TheNovemberMan_Poster-637x920.jpg' alt='Novemberman' width='100' heigth='200'>"],
+        ["<strong>Novemberman</strong><br/> <img onmousedown=\"javascript:ShowOverlay('NovemberMan')\" src='http://m.929jackfm.ca/files/VVS_TheNovemberMan_Poster-637x920.jpg' alt='Novemberman' width='100' heigth='200'>"],
         ["<strong>Let's be cops</strong><Br/> <img onmousedown=\"javascript:ShowOverlay('Lets be cops')\" src='http://www.pathe.nl/thumb/180x252/gfx_content/posters/letsbecops3.jpg' alt='Lets be cops' width='100' heigth='200'>"],
         ["<strong>As Above So Below</strong><Br/> <img onmousedown=\"javascript:ShowOverlay('As Above So Below')\" src='http://www.pathe.nl/thumb/180x252/gfx_content/posters/asabove500(1).jpg' alt='As Above So Below' width='100' heigth='200'>"],
         ["<strong>Guardians Of The Galaxy</strong><Br/> <img onmousedown=\"javascript:ShowOverlay('Guardians Of The Galaxy')\" src='http://www.pathe.nl/thumb/180x252/gfx_content/posters/guardiansofthegalaxy1.jpg' alt='Guardians Of The Galaxy' width='100' heigth='200'>"],
@@ -64,8 +63,8 @@ function LoadAgenda() {
 }
 function ShowOverlay(FILM) {
     var output = document.getElementById("overlay");
-    var width = screen.width - 300;
-    var height = screen.height - 300;
+    var width = screen.width - 500;
+    var height = screen.height - 500;
     switch (FILM) {
         case 'Finding Fanny':
             output.innerHTML = '<iframe id="promo"  width="' + width + '" height="' + height + '" src="https://www.youtube.com/embed/5O86FUOrlkY"frameborder="0" allowfullscreen></iframe>\n\
@@ -121,7 +120,7 @@ function ShowOverlay(FILM) {
                     ;
             document.getElementById("overlay").style.display = "block";
             break;
-        case 'Lets Be Cops':
+        case 'Lets be cops':
             output.innerHTML = '<iframe id="promo"  width="' + width + '" height="' + height + '" src="https://www.youtube.com/embed/UKIAZjs__Xc"frameborder="0" allowfullscreen></iframe>\n\
                                 <p onmousedown="CloseOverlay()" style="color:white; float:right;">X</p>\n\
                                 <br/><center>\n\
