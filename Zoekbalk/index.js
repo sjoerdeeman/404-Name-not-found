bdb4e89e02696ec7bbbdd67b4c1fd4f4510e4c2b
M138A <0896375@hr.nl>
2014-10-29 11:18:36 +0100

    A Few Changes

--- Zoekbalk/index.js
+++ Zoekbalk/index.js
@@ -1,7 +1,12 @@
 function searchq()  {
     var searchTxt = $("input[name='searchVal']").val();
-    
+    if (searchTxt == "" && searchTxt.length < 2){
+        document.getElementById("output").innerHTML = "";
+    }
+    else{
 	$.post("Zoekbalk/index.php", {searchVal: searchTxt}, function(output)  {
 	  $("#output").html(output);
 	});
+    
+    }
 }
