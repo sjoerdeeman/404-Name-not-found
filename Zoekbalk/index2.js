c3849e2b0365bc4aed2c69f552233897ff81eb6b
TimNijssenKAAS <tim.nijssen@live.nl>
2014-10-29 10:22:31 +0100

    positie zoekbalk fix

--- Zoekbalk/index2.js
+++ Zoekbalk/index2.js
@@ -1,13 +1,13 @@
 $(document).ready(function searchq()  {
     $('.searchVal').keyup(function() {
 	var searchTxt = $(this).attr('value');
-    	$.post("index.php", {searchVal:searchVal}, function(data)  {
+    	$.post("index2.php", {searchVal:searchVal}, function(data)  {
 	  $('.result').html(data);
 	  
 	    $('.result li').click(function)()  {
 		  var result_value= $(this).text();
 		  $('.searchval').attr('value', result_value);
-		  $(',
+		  
 		  $('.result').html('')
 		
 		});
